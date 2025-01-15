564706a08159e843fcfee4b18d5b23068b55018e
AutumnAround <autumnaroundyou@gmail.com>
2025-01-15 20:35:34 +0000

    Добавил 2 задания

---
+++ declareArrowFunction.js
@@ -0,0 +1,13 @@
+/*
+Оголоси функцію-стрілку getString, яка не приймає ніяких параметрів.
+
+Всередині функції getString:
+
+оголоси змінну jobTitle і присвой їй значення - рядок 'I am a programmer'.
+поверни змінну jobTitle за допомогою ключового слова return.
+Примітка: не використовуй ключове слово function у цьому завданні.
+*/
+let getString = () => {
+    let jobTitle = 'I am a programmer';
+    return jobTitle;
+};
